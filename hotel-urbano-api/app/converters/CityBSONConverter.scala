b01a5bb17d1498fd0380e871dfbb6f151f3f5782
Leon Maia <hi@leonmaia.com>
2015-01-31 02:17:50 -0300

    City model and BSONConverter

--- hotel-urbano-api/app/converters/CityBSONConverter.scala
+++ hotel-urbano-api/app/converters/CityBSONConverter.scala
@@ -8,4 +8,25 @@ import java.util.UUID
 
 object CityBSONConverter extends BSONDocumentWriter[City] with BSONDocumentReader[City] {
 
+  override def write(city: City): BSONDocument = {
+    val baseDoc = BSONDocument(      
+      "cityName" -> city.name
+    )
+    val optionalFields = Seq(
+      ("cityId", city.id.map(id => BSONString(id.toString)))
+    )
+   
+    optionalFields.foldLeft(baseDoc) { case (bson, (key, optionalValue)) =>
+      optionalValue match {
+        case Some(value) => bson ++ BSONDocument(Seq(key -> value))
+        case None => bson
+      }
+    }
+  }
+
+  override def read(bson: BSONDocument): City = {
+    City(bson.getAs[String]("cityId").map(id => UUID.fromString(id)),
+      bson.getAs[String]("cityName").get
+    )
+  }
 }
--- hotel-urbano-api/app/models/City.scala
+++ hotel-urbano-api/app/models/City.scala
@@ -8,6 +8,17 @@ import converters.writes._
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
-object City {}
+object City {
+  implicit val cityWriter: Writes[City] = (
+    (JsPath \ "cityId").writeNullable[UUID] and
+    (JsPath \ "cityName").write[String]
+  )(unlift(unapply))
 
-case class City()
+  implicit val cityReader: Reads[City] = (
+    (JsPath \ "cityId").readNullable[UUID] and
+    (JsPath \ "cityName").read[String]
+  )(apply _)
+}
+
+case class City(val id: Option[UUID],
+                val name: String)
