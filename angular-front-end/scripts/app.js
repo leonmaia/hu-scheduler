c81e03e86a3488f7e31df4eafddf8f768b828f5a
Leon Maia <hi@leonmaia.com>
2015-02-25 19:25:33 -0300

    select dates

--- angular-front-end/scripts/app.js
+++ angular-front-end/scripts/app.js
@@ -2,26 +2,24 @@
   var app = angular.module('scheduler', ['angularPikaday']);
 
   app.controller("SchedulerController", ['$scope', '$filter', '$http', function($scope, $filter, $http) {
+
     $http.get('api/hotels').then(function(result) {
       $scope.hotels = result.data.hotels;
     });
 
-    $scope.search = function() {
-      var checkin = new Date($scope.query.dataIda);
-      var checkout = new Date($scope.query.dataVolta);
+    $scope.$watch('query.dataIda', function() {
+      if ($scope.dataVoltaObject != undefined)
+        $scope.dataVoltaObject.setMinDate($scope.dataIdaObject.getDate());
+    });
 
-      if (checkin < checkout) {
-        var checkinDate = $filter('date')(checkin, "dd/MM/yyyy");
-        var checkoutDate = $filter('date')(checkout, "dd/MM/yyyy");
-        getCityId($scope.query.cidade, checkinDate, checkoutDate);
-      } else {
-        alert("Voce nao pode ir embora antes de chegar.");
-      }
+    $scope.search = function() {
+      var checkinDate = $filter('date')($scope.dataIdaObject.getDate(), "dd/MM/yyyy");
+      var checkoutDate = $filter('date')($scope.dataVoltaObject.getDate(), "dd/MM/yyyy");
+      getCityId($scope.query.cidade, checkinDate, checkoutDate);
     };
 
     var getCityId = function(query,checkinDate,checkoutDate) {
       $http.get('api/cities?name='+query).then(function(result) {
-        debugger;
         var cityId = result.data.cities[0].cityId
         var getUrl = 'api/hotels?city='+cityId+'&checkin='+checkinDate+'&checkout='+checkoutDate;
         $http.get(getUrl).then(function(result) {
